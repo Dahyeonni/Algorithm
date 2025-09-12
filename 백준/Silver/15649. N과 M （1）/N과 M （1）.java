384fa635f93ccc423cd95521be9df3dbd3b3fde0
DaHyeon Yoo <102225686+Dahyeonni@users.noreply.github.com>
2025-09-12 13:47:44 +0900

    [Silver III] Title: N과 M (1), Time: 324 ms, Memory: 26800 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,53 +1,55 @@
 import java.util.Scanner;
 
 public class Main {
-	
+
 	static int N;
 	static int M;
-	static int[] arr, res;
-	static boolean isSelected[];
-	
-	
+	static int[] data;
+	static int[] sel;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		N = sc.nextInt();
 		M = sc.nextInt();
-		
-		arr = new int[N];
-        res = new int[M];
-        isSelected = new boolean[N];
-        
-		for(int i = 0; i < N; i++) 
-			arr[i] = i+1;
-		
-		perm(0);
 
+		data = new int[N];
+		sel = new int[M];
+		visited = new boolean[N];
+
+		for (int i = 0; i < N; i++) {
+			data[i] = i + 1;
+		}
+
+		// 순열
+		perm(0);
+		System.out.println(sb);
 	}
-	
+
 	static void perm(int idx) {
-		// 기저조건
-		if(idx == M) {
-			for(int n : res) {
-				System.out.print(n+ " ");
-				
+		if (idx == M) {
+			for (int s : sel) {
+				sb.append(s).append(" ");
+
 			}
-			System.out.println();
-			
+			sb.append("\n");
 			return;
 		}
-		for(int i = 0; i < N; i++) {
-			if(isSelected[i]) continue;
-			
-			res[idx] = arr[i];
-			isSelected[i] = true;
-			perm(idx+1);
-			isSelected[i] = false;
-			
-		}
 
+		for (int i = 0; i < N; i++) {
+
+			if (!visited[i]) {
+				sel[idx] = data[i];
+				visited[i] = true;
+				perm(idx + 1);
+				visited[i] = false;
+			}
+
+		}
 	}
 
 }
