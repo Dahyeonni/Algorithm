8b57554ad9530ac44f78582fa2aef44a563bfe3a
DaHyeon Yoo <102225686+Dahyeonni@users.noreply.github.com>
2025-09-16 09:24:43 +0900

    [Bronze V] Title: A+B - 3, Time: 196 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		for (int t = 0; t < T; t++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+
+			System.out.println(A + B);
+		}
+
+	}
+
+}
