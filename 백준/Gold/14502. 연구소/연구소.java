6a5d7c4dc78e10157fb280d2c99357e284d6dbfc
DaHyeon Yoo <102225686+Dahyeonni@users.noreply.github.com>
2025-09-27 23:02:25 +0900

    [Gold IV] Title: 연구소, Time: 396 ms, Memory: 83912 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -11,6 +10,7 @@ public class Main {
 	static int N, M;
 
 	static int[][] map;
+	static int firstBlank = 0;
 	static int MAX = -1; // 계속 비교 되어야 하니까 전역
 
 	static int[] dr = { 1, -1, 0, 0 };
@@ -30,82 +30,72 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 0)
+					firstBlank++; // 초기빈칸
 			}
 		} // 입력완료
 
-		threewall(0,0);
+		threewall(0, 0);
 
 		System.out.println(MAX);
 
 	}
 
+	static void threewall(int start, int cnt) {
+		if (cnt == 3) {
+			virus();
+			return;
+		}
+		// 평탄화 작업
+		for (int idx = start; idx < N * M; idx++) {
+			int r = idx / M;
+			int c = idx % M;// 둘다 M 인 것에 주의
 
-	
-    static void threewall(int start, int cnt) {
-        if (cnt == 3) {
-            virus();
-            return;
-        }
-
-        for (int idx = start; idx < N * M; idx++) {
-            int r = idx / M;
-            int c = idx % M;
-
-            if (map[r][c] == 0) {
-                map[r][c] = 1;
-                threewall(idx + 1, cnt + 1);
-                map[r][c] = 0;
-            }
-        }
-    }
+			if (map[r][c] == 0) {
+				map[r][c] = 1;
+				threewall(idx + 1, cnt + 1);
+				map[r][c] = 0;
+			}
+		}
+	}
 
 	static void virus() {
 		Queue<int[]> q = new ArrayDeque<>();
-		// 원본 훼손 ㄴㄴ
-		int[][] mapCopy = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			mapCopy[i] = Arrays.copyOf(map[i], M);
-		}
+		boolean[][] visited = new boolean[N][M];
 
-		// 2 인 부분 넣어야지
+		int infected = 0;
+
+		// 바이러스 위치 전부 큐에 넣기
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (mapCopy[i][j] == 2) {
+				if (map[i][j] == 2) {
 					q.offer(new int[] { i, j });
+					visited[i][j] = true; // 기존 바이러스는 방문처리
 				}
 			}
 		}
 
+		// BFS로 바이러스 퍼뜨리기
 		while (!q.isEmpty()) {
-
-			int[] curr = q.poll();
-
-			int currR = curr[0];
-			int currC = curr[1];
+			int[] cur = q.poll();
+			int r = cur[0], c = cur[1];
 
 			for (int d = 0; d < 4; d++) {
-				int nr = dr[d] + currR;
-				int nc = dc[d] + currC;
+				int nr = r + dr[d];
+				int nc = c + dc[d];
 
 				if (nr >= 0 && nr < N && nc >= 0 && nc < M) {
-					if (mapCopy[nr][nc] == 0) {
-						mapCopy[nr][nc] = 2; // 퍼트리기
+					if (!visited[nr][nc] && map[nr][nc] == 0) {
+						visited[nr][nc] = true;
+						infected++;
 						q.add(new int[] { nr, nc });
 					}
 				}
 			}
 		}
-		// 안전영역 확인
-		int clear = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (mapCopy[i][j] == 0)
-					clear++;
-			}
-		}
 
-		MAX = Math.max(MAX, clear);
-
+		// 첫빈칸 - 벽3- 바이러스
+		int safe = firstBlank - 3 - infected;
+		MAX = Math.max(MAX, safe);
 	}
-
 }
