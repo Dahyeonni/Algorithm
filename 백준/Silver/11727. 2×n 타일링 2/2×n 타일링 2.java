74b321d774c7f75f37a7bb400c016889197d0ad4
DaHyeon Yoo <102225686+Dahyeonni@users.noreply.github.com>
2025-09-27 18:15:02 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static int[] dp;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+
+		dp = new int[N + 1];
+
+		dp[1] = 1;
+		if (N >= 2)
+			dp[2] = 3;
+
+		for (int i = 3; i <= N; i++) {
+			dp[i] = (dp[i - 1] + 2*(dp[i - 2])) % 10007;
+		}
+
+		System.out.println(dp[N]);
+
+	}
+
+}
