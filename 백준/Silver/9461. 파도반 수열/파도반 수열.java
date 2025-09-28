a912c71b6f0ff6a313d83714bfed4d153409703a
DaHyeon Yoo <102225686+Dahyeonni@users.noreply.github.com>
2025-09-28 21:57:47 +0900

    [Silver III] Title: 파도반 수열, Time: 188 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static long[] dp = new long[101];
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		solve();
+		
+		for(int t = 0; t < T; t++) {
+			int N = sc.nextInt();
+			System.out.println(dp[N]);
+		}
+	}
+	
+	static void solve() {
+		dp[1] = 1;
+		dp[2] = 1;
+		dp[3] = 1;
+		dp[4] = 2;
+		dp[5] = 2;
+	
+		for(int i = 6; i <= 100; i++) {
+			dp[i] = dp[i-1] + dp[i-5];
+		}
+	}
+
+}
