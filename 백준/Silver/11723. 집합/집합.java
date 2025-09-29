8a878937e018dcbded3275e801cf866f76cde845
DaHyeon Yoo <102225686+Dahyeonni@users.noreply.github.com>
2025-09-29 23:25:36 +0900

    [Silver V] Title: 집합, Time: 1508 ms, Memory: 312900 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int M = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+
+            if (cmd.equals("add")) {
+                int x = Integer.parseInt(st.nextToken());
+                set.add(x);
+            } else if (cmd.equals("remove")) {
+                int x = Integer.parseInt(st.nextToken());
+                set.remove(x);
+            } else if (cmd.equals("check")) {
+                int x = Integer.parseInt(st.nextToken());
+                sb.append(set.contains(x) ? 1 : 0).append("\n");
+            } else if (cmd.equals("toggle")) {
+                int x = Integer.parseInt(st.nextToken());
+                if (set.contains(x)) set.remove(x);
+                else set.add(x);
+            } else if (cmd.equals("all")) {
+                set.clear();
+                for (int j = 1; j <= 20; j++) set.add(j);
+            } else if (cmd.equals("empty")) {
+                set.clear();
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
