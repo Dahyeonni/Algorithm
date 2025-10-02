3fae78810534e8ba1e76be6a7898477cee2ca969
DaHyeon Yoo <102225686+Dahyeonni@users.noreply.github.com>
2025-10-03 00:09:23 +0900

    [Silver IV] Title: 동전 0, Time: 176 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+
+		int[] coins = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			coins[i] = sc.nextInt();
+		}
+
+		int cnt = 0;
+
+		for (int i = N - 1; i >= 0; i--) {
+			if (coins[i] <= K) {
+				cnt += K / coins[i];
+				K %= coins[i];
+			}
+		}
+
+		System.out.println(cnt);
+
+	}
+
+}
