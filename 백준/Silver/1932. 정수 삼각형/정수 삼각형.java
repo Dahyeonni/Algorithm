666b9119fca47f090ef8c3110051ece7c21c6222
DaHyeon Yoo <102225686+Dahyeonni@users.noreply.github.com>
2025-10-22 11:47:44 +0900

    [Silver I] Title: 정수 삼각형, Time: 260 ms, Memory: 27176 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -14,12 +14,12 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 
-		arr = new int[N][N];
-		dp = new int[N][N];
+		arr = new int[N + 1][N + 1];
+		dp = new int[N + 1][N + 1];
 
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < i + 1; j++) {
+			for (int j = 1; j <= i; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		} // 배열 입력 완료
@@ -27,26 +27,17 @@ public class Main {
 		MaxSum();
 
 		int ans = 0;
-		for (int i = 0; i < N; i++) {
-			ans = Math.max(ans, dp[N - 1][i]);
+		for (int i = 1; i <= N; i++) {
+			ans = Math.max(ans, dp[N][i]);
 		}
 
 		System.out.println(ans);
 	}
 
 	static void MaxSum() {
-		dp[0][0] = arr[0][0]; // 최상위 노드의 값
-		for (int i = 1; i < N; i++) {
-			for (int j = 0; j < i + 1; j++) {
-
-				if (j == 0)
-					dp[i][j] = arr[i][j] + dp[i - 1][j];
-
-				else if (j == i)
-					dp[i][j] = arr[i][j] + dp[i - 1][j - 1];
-
-				else
-					dp[i][j] = arr[i][j] + Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++) {
+				dp[i][j] = arr[i][j] + Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
 			}
 		}
 	}
